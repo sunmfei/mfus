b119c06ee83ecf2015d1ea0e2dc9a4a34cd88914
sunmfei <109935008+sunmfei@users.noreply.github.com>
2023-03-11 19:47:17 +0800

    Create init.go

---
+++ start/init.go
@@ -0,0 +1,36 @@
+package start
+
+import (
+	"github.com/sunmfei/mfus/utils/commonUtils"
+	"github.com/sunmfei/mfus/utils/dbutils"
+	"github.com/sunmfei/mfus/utils/dbutils/redisUtil"
+	"gorm.io/gorm"
+)
+
+func Page(workDir string, typ string, info interface{}) (*gorm.DB, *redisUtil.RedisPlay) {
+	Setup(workDir)
+	var db *gorm.DB
+	db = GetDB(typ)
+	autoTable(db, info)
+	return db, GetRedis()
+}
+func ReadConf(workDir string) {
+	//读取配置文件
+	commonUtils.ReadConf(&workDir)
+}
+func Setup(workDir string) {
+	//读取配置文件
+	commonUtils.ReadConf(&workDir)
+	initLogger()
+
+}
+func AutoTable(db *gorm.DB, info interface{}) {
+	autoTable(db, info)
+}
+func GetDB(typ string) *gorm.DB {
+	return dbutils.Setup(typ)
+}
+
+func GetRedis() *redisUtil.RedisPlay {
+	return redisUtil.Setup()
+}
